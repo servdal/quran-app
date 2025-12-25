b9976519c4749845ecc01fa30c53e2b281d36b36
Duidev Software <swandhana17@gmail.com>
2025-12-25 18:14:15 +0700

    update

--- lib/models/audio_index.dart
+++
@@ -1,9 +0,0 @@
-class AudioIndex {
-  final String file;
-
-  AudioIndex({required this.file});
-
-  factory AudioIndex.fromJson(Map<String, dynamic> json) {
-    return AudioIndex(file: json['file']);
-  }
-}
--- lib/models/grammar_type.dart
+++
@@ -1,9 +0,0 @@
-enum GrammarType { fiil, isim, harf, other }
-
-GrammarType detectGrammarType(String desc) {
-  final d = desc.toLowerCase();
-  if (d.contains('verb')) return GrammarType.fiil;
-  if (d.contains('noun')) return GrammarType.isim;
-  if (d.contains('particle')) return GrammarType.harf;
-  return GrammarType.other;
-}

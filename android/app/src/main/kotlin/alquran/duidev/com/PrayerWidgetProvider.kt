db486d2feb3aa5474f2be32c4c50725a8c07377c
Dwi Swandhana <swandhana17@gmail.com>
2026-03-15 13:06:28 +0700

    update

--- android/app/src/main/kotlin/alquran/duidev/com/PrayerWidgetProvider.kt
+++ android/app/src/main/kotlin/alquran/duidev/com/PrayerWidgetProvider.kt
@@ -2,44 +2,54 @@ package alquran.duidev.com
 
 import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
-import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.util.Log
 import android.widget.RemoteViews
+import android.content.SharedPreferences
+import es.antonborri.home_widget.HomeWidgetProvider
 
-class PrayerWidgetProvider : AppWidgetProvider() {
+class PrayerWidgetProvider : HomeWidgetProvider() {
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray,
+        widgetData: SharedPreferences,
     ) {
+        Log.d("PrayerWidgetProvider", "onUpdate ids=${appWidgetIds.size}")
         for (appWidgetId in appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId)
+            updateAppWidget(context, appWidgetManager, appWidgetId, widgetData)
         }
     }
 
     companion object {
-        private const val PREFS = "HomeWidgetPreferences"
-
         fun updateAppWidget(
             context: Context,
             appWidgetManager: AppWidgetManager,
             appWidgetId: Int,
+            widgetData: SharedPreferences,
         ) {
-            val prefs = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
-
             val views = RemoteViews(context.packageName, R.layout.prayer_widget)
-            views.setTextViewText(R.id.tv_location, prefs.getString("location", "") ?: "")
-            views.setTextViewText(R.id.tv_hijri, prefs.getString("hijriDate", "") ?: "")
-            views.setTextViewText(R.id.tv_next_name, prefs.getString("closestPrayer", "") ?: "")
-            views.setTextViewText(R.id.tv_next_time, prefs.getString("closestTime", "") ?: "")
+            val location = widgetData.getString("location", "") ?: ""
+            val hijri = widgetData.getString("hijriDate", "") ?: ""
+            val nextName = widgetData.getString("closestPrayer", "") ?: ""
+            val nextTime = widgetData.getString("closestTime", "") ?: ""
+            views.setTextViewText(R.id.tv_location, location)
+            views.setTextViewText(R.id.tv_hijri, hijri)
+            views.setTextViewText(R.id.tv_next_name, nextName)
+            views.setTextViewText(R.id.tv_next_time, nextTime)
 
-            views.setTextViewText(R.id.tv_fajr, prefs.getString("Fajr", "--:--") ?: "--:--")
-            views.setTextViewText(R.id.tv_dhuhr, prefs.getString("Dhuhr", "--:--") ?: "--:--")
-            views.setTextViewText(R.id.tv_asr, prefs.getString("Asr", "--:--") ?: "--:--")
-            views.setTextViewText(R.id.tv_maghrib, prefs.getString("Maghrib", "--:--") ?: "--:--")
-            views.setTextViewText(R.id.tv_isha, prefs.getString("Isha", "--:--") ?: "--:--")
+            views.setTextViewText(R.id.tv_fajr, widgetData.getString("Fajr", "--:--") ?: "--:--")
+            views.setTextViewText(R.id.tv_dhuhr, widgetData.getString("Dhuhr", "--:--") ?: "--:--")
+            views.setTextViewText(R.id.tv_asr, widgetData.getString("Asr", "--:--") ?: "--:--")
+            views.setTextViewText(R.id.tv_maghrib, widgetData.getString("Maghrib", "--:--") ?: "--:--")
+            views.setTextViewText(R.id.tv_isha, widgetData.getString("Isha", "--:--") ?: "--:--")
+
+            Log.d(
+                "PrayerWidgetProvider",
+                "update widgetId=$appWidgetId location='$location' next='$nextName $nextTime'",
+            )
 
             val intent = Intent(context, MainActivity::class.java)
             val flags =
@@ -51,4 +61,3 @@ class PrayerWidgetProvider : AppWidgetProvider() {
         }
     }
 }
-
